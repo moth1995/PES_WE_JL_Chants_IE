89c6e90af6a98990278d124875893e709aa15937
moth1995 <marcos-flores@live.com.ar>
2023-01-02 10:33:40 -0300

    update on make.bat

--- make.bat
+++ make.bat
@@ -5,7 +5,7 @@ set VERSION=1.0.0
 set FILE_VERSION=file_version_info.txt
 set ICO_DIR=pes_indie.ico
 
-pyinstaller --onefile --window "%PY_FILE%" --icon="%ICO_DIR%" --name "%PROJECT_NAME%_%VERSION%" --version-file "%FILE_VERSION%"
+pyinstaller --onefile "%PY_FILE%" --icon="%ICO_DIR%" --name "%PROJECT_NAME%_%VERSION%" --version-file "%FILE_VERSION%"
 
 cd dist
 tar -acvf "%PROJECT_NAME%_%VERSION%.zip" * config
